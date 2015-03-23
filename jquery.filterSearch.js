31360a6e6ca72e972c47f123ead678d9fba9bde7
Drew Smith <drewstiff@gmail.com>
2015-03-23 10:19:44 +0000

    First commit

---
+++ jquery.filterSearch.js
@@ -0,0 +1,35 @@
+
+jQuery.fn.filterSearch = function (list) {
+
+    list = jQuery(list).children();
+    var input = this;
+
+    input.change(function () {
+        var filter = input.val().toLowerCase().split(" ").filter(function (el) { return el.length != 0 });
+        list.each(function () {
+            var item = jQuery(this);
+            var text = item.text() || "";
+
+            function isFilterInItem(filter) {
+                if (text.toLowerCase().indexOf(filter) >= 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            if (filter.every(isFilterInItem)) {
+                item.show();
+            } else {
+                item.hide();
+            }
+        })
+        return false;
+    }).keyup(function () {
+        input.change();
+    });
+
+    input.change();
+
+    return this; // maintain jQuery chainability
+}
